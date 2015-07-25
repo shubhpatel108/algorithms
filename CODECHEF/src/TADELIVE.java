228acba340a95112128b41a809df0b8fafd32895
Shubham Patel <shubhpatel108@gmail.com>
2015-07-25 19:00:00 +0530

    Added TADELIVE.java

---
+++ CODECHEF/src/TADELIVE.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TADELIVE {
+	public static void main(String arg[]) throws Exception
+	{
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
