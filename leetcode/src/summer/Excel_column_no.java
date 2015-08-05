10bb4927c4cb6343afb4a7358d6e1b38f3ed60bb
Shubham Patel <shubhpatel108@gmail.com>
2015-08-05 20:00:00 +0530

    Added Excel_column_no.java

---
+++ leetcode/src/summer/Excel_column_no.java
@@ -0,0 +1,27 @@
+package summer;
+
+public class Excel_column_no {
+	public static void main(String args[])throws Exception
+	{
+		System.out.println(titleToNumber("BA"));
+	}
+	public static int titleToNumber(String s) {
+		int len = s.length();
+		int ans = 0;
+		for(int i=1;i<len;i++) {
+			ans += (int) Math.pow(26, i);
+		}
+		for(int i=0;i<len;i++)
+		{
+			ans += (totalBack(s.charAt(i)) * (int)Math.pow(26, len-1-i));
+		}
+		return ans + 1;
+	}
+	public static int charToNo(char c) {
+		return (int)(c)-64;
+	}
+	public static int totalBack(char c) {
+		return charToNo(c) - 1 ;
+	}
+	
+}
