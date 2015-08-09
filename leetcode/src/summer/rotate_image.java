e84c1957114b41094f76da43bc66e9a30ffbbea2
Shubham Patel <shubhpatel108@gmail.com>
2015-08-09 20:00:00 +0530

    Added rotate_image.java

---
+++ leetcode/src/summer/rotate_image.java
@@ -0,0 +1,31 @@
+package summer;
+
+public class rotate_image {
+	public static void main(String args[])throws Exception
+	{
+		int[][] matrix = {{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+		rotate(matrix);
+	}
+	public static void rotate(int[][] matrix) {
+		if(matrix.length==0)
+			return;
+        int max = matrix.length-1;
+        int min = 0;
+        int temp;
+        
+        while(min<max)
+        {
+        	int length = max - min;
+        	for(int i=0;i<length;i++)
+        	{
+        		temp = matrix[min][min+i];
+        		matrix[min][min+i] = matrix[max-i][min];
+        		matrix[max-i][min] = matrix[max][max-i];
+        		matrix[max][max-i] = matrix[min+i][max];
+        		matrix[min+i][max] = temp;
+    		}
+        	min++;
+        	max--;
+        }
+    }
+}
