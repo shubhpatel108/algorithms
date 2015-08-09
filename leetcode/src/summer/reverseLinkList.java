d86215f5f2a8a70e3e6d13e97b161a29662a085d
Shubham Patel <shubhpatel108@gmail.com>
2015-08-09 19:00:00 +0530

    Added reverseLinkList.java

---
+++ leetcode/src/summer/reverseLinkList.java
@@ -0,0 +1,16 @@
+package summer;
+
+public class reverseLinkList {
+	public static void main(String args[])throws Exception
+	{
+		LinkList ll = new LinkList();
+		ll.addNode(1);
+		ll.addNode(2);
+		ll.addNode(3);
+		ll.addNode(4);
+		ll.addNode(5);
+		ll.printList(ll.head);
+		ll.reverse(ll.head);
+		ll.printList(ll.head);
+	}
+}
