77a5a15709845db8c81cbfb1e679acd849a0dccc
Shubham Patel <shubhpatel108@gmail.com>
2015-08-09 22:00:00 +0530

    Added simple_max_profit.java

---
+++ leetcode/src/summer/simple_max_profit.java
@@ -0,0 +1,21 @@
+package summer;
+
+public class simple_max_profit {
+	
+	public static void main(String args[]) {
+		int array[] = {1,1,1,1};
+		System.out.println(maxProfit(array));
+	}
+	
+	public static int maxProfit(int[] prices) {
+		if(prices.length<2)
+			return 0;
+		
+		int diff = 0;
+		int min = prices[0];
+		for(int i=0;i<prices.length;i++)
+		{
+			
+		}
+	}
+}
