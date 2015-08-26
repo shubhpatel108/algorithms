4a7ac8967547011d0efd862209a74dafa4225bff
Shubham Patel <shubhpatel108@gmail.com>
2015-08-26 19:00:00 +0530

    Added template2.java

---
+++ Codeforces/src/template2.java
@@ -0,0 +1,14 @@
+//One integer input
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class template2 {
+	public static void main(String args[])throws Exception {
+		BufferedReader input = new BufferedReader(
+				new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(input.readLine());
+		
+		System.out.println();
+	}
+}
