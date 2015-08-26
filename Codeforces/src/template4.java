c606d60dffbb9a0792428f23d003b534a1662d6e
Shubham Patel <shubhpatel108@gmail.com>
2015-08-26 21:00:00 +0530

    Added template4.java

---
+++ Codeforces/src/template4.java
@@ -0,0 +1,18 @@
+//t test cases using while
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class template4 {
+	public static void main(String args[])throws Exception {
+		BufferedReader input = new BufferedReader(
+				new InputStreamReader(System.in));
+		String[] tokens = input.readLine().split(" ");
+		
+		int t = Integer.parseInt(tokens[0]);
+		
+		while(t-->0) {
+			
+		}
+	}
+}
