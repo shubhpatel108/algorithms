a7c5956cdabe20ed8c6d282d0a505c22c2f66cdc
Shubham Patel <shubhpatel108@gmail.com>
2015-08-31 20:00:00 +0530

    Added D579.java

---
+++ Codeforces/src/C320/D579.java
@@ -0,0 +1,18 @@
+package C320;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class D579 {
+	public static void main(String args[])throws Exception {
+		BufferedReader input = new BufferedReader(
+				new InputStreamReader(System.in));
+		String[] tokens = input.readLine().split(" ");
+		
+		int t = Integer.parseInt(tokens[0]);
+		
+		while(t-->0) {
+			
+		}
+	}
+}
