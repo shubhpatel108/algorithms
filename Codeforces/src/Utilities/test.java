27ccd301698fefa5e908163a1c73c396cbb49230
Shubham Patel <shubhpatel108@gmail.com>
2015-09-05 21:00:00 +0530

    Added test.java

---
+++ Codeforces/src/Utilities/test.java
@@ -0,0 +1,37 @@
+package Utilities;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class test {
+	public static void main(String args[])throws Exception {
+//		ArrayList<Integer> list = new ArrayList<Integer>();
+//		
+//		list.add(1);
+//		list.add(2);
+//		
+//		System.out.println(list.get(0));
+//		
+//		List<Integer> list = new ArrayList<Integer>();
+//		list.add(1);
+//		list.add(2);
+//		list.add(3);
+//
+//		List<Integer> list2 = new ArrayList<Integer>();
+////		list.add(1);
+//		list.add(2);
+////		
+//		System.out.println(Collections.indexOfSubList(list, list2));
+//		int []arr = {1,2,3};
+		System.out.println(Math.log(1)/Math.log(2) * 2);
+		PriorityQueue<Integer> q = new PriorityQueue<Integer>();
+		
+	}
+}
