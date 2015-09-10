c1ddde6e949b52bd9f901625585d47bd67a51f50
Shubham Patel <shubhpatel108@gmail.com>
2015-09-10 19:00:00 +0530

    Added B520.java

---
+++ Codeforces/src/graphs/B520.java
@@ -0,0 +1,19 @@
+//INC
+package graphs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B520 {
+	static int[] efforts;
+	public static void main(String args[])throws Exception {
+		BufferedReader input = new BufferedReader(
+				new InputStreamReader(System.in));
+		String[] tokens = input.readLine().split(" ");
+		
+		int n = Integer.parseInt(tokens[0]);
+		int target = Integer.parseInt(tokens[1]);
+		
+	}
+
+}
