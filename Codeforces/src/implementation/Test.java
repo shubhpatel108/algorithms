9cdb5f1cdd465354b571d5140fc0abb02fad2a12
Shubham Patel <shubhpatel108@gmail.com>
2015-09-15 21:00:00 +0530

    Added Test.java

---
+++ Codeforces/src/implementation/Test.java
@@ -0,0 +1,10 @@
+package implementation;
+
+import Utilities.gcd;
+
+public class Test {
+	public static void main(String[] args)throws Exception {
+		gcd g = new gcd();
+		System.out.println(g.gcd(5, 5));
+	}
+}
