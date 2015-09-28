458925e0b8b5468bd5aa191194de8d707e9051f4
Shubham Patel <shubhpatel108@gmail.com>
2015-09-28 14:00:00 +0530

    Added template.java

---
+++ CodeJam/src/template.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class template {
+	public static void main(String[] args) throws Exception {
+		Scanner input = new Scanner(System.in);
+		int T = input.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			int n = input.nextInt();
+			
+			for(int i=0; i<n; i++) {
+				
+			}
+			System.out.println("Case #"+t+": ");
+		}
+	}
+}
