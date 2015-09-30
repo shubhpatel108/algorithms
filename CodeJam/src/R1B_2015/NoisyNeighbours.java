efaaa686d1898cc1a4d89f25d338184fbc0b18d1
Shubham Patel <shubhpatel108@gmail.com>
2015-09-30 15:00:00 +0530

    Added NoisyNeighbours.java

---
+++ CodeJam/src/R1B_2015/NoisyNeighbours.java
@@ -0,0 +1,23 @@
+//https://code.google.com/codejam/contest/8224486/dashboard#s=p1
+
+package R1B_2015;
+
+import java.util.Scanner;
+
+public class NoisyNeighbours {
+	public static void main(String[] args) throws Exception {
+		Scanner input = new Scanner(System.in);
+		int T = input.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			int r = input.nextInt();
+			int c = input.nextInt();
+			int z = input.nextInt();
+			
+			for(int i=0; i<=n; i++) {
+				
+			}
+			System.out.println("Case #"+t+": ");
+		}
+	}
+}
