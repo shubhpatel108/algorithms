66cd6a273ca25b3034fa8f47ac21a23499ced495
Shubham Patel <shubhpatel108@gmail.com>
2015-10-18 15:00:00 +0530

    Added Node.java

---
+++ HackerEarth/src/SegmentTree/Node.java
@@ -0,0 +1,13 @@
+package SegmentTree;
+
+public class Node {
+	String value;
+	Node leftChild;
+	Node rightChild;
+	int start;
+	int end;
+	
+	public Node(String value) {
+		this.value = value;
+	}
+}
